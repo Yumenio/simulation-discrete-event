fc11afc10e1700ad103e1bc141d352b476418465
Yumenio <alejandroandy447@gmail.com>
2021-11-17 16:18:02 -0500

    normal randomvar gen

--- va.py
+++ va.py
@@ -1,5 +1,5 @@
 from random import uniform
-from math import log
+from math import log,sqrt
 
 def uni(a,b):
   u = uniform(0,1)
@@ -14,3 +14,15 @@ def exp(l):
   except: # in case log(u) goes wrong
     return exp(l)
 
+def norm(mu=0,sigma=1):
+  u = uniform(0,1)
+  y1 = 1
+  y2 = 0
+  while y2 - ( ( (y1-1)**2 ) /2 ) <= 0:
+    y1 = exp(1)
+    y2 = exp(1)
+
+  u = uniform(0,1)
+  ans = y1 if u > 0.5 else -y1
+
+  return ans*sqrt(sigma) + mu
